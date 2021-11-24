074b348d0c40b69c4834a743aa363aaf897a3f4a
Rafael Augusto <rafaelaugusto.developer@gmail.com>
2021-11-24 11:45:26 -0300

    heroku

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,6 @@
 import server from './config/express'
 
 const app = server()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`Server is running in ${port}`))
